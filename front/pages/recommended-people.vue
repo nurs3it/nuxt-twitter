a7518cc17bb5bcd932caac19e2a5b0d6ac6b6ef5
nurseit <ns.ab06041999@gmail.com>
2020-09-22 18:25:15 +0600

    updates

--- front/pages/recommended-people.vue
+++ front/pages/recommended-people.vue
@@ -1,13 +1,47 @@
 <template>
-  <div class="recommended-people"></div>
+  <div class="recommended-people">
+    <div class="recommended-people__title">
+      <p class="ui-large-text"><b>Кого читать</b></p>
+    </div>
+    <div class="recommended-people__list">
+      <div class="recommended-people__list__item" v-for="item in users" :key="item.id">
+        <div class="recommended-people__list__item__icon">
+          <i class="material-icons mr-10 md-48 cursor-pointer">face</i>
+        </div>
+        <div class="recommended-people__list__item__info user-name">
+          <div class="user-name__name">
+            <p class="ui-large-text text-bolder">{{ item.name }}</p>
+            <small class="ui-small-text">{{ item.email }}</small>
+          </div>
+          <div class="user-name__btn">
+            <u-i-button />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import API from "@/API/api.services"
+import UserInRecommended from "@/components/UserInRecommended";
+import UIButton from "@/components/UIButton";
+
 export default {
-name: "recommended-people"
+  name: "recommended-people",
+  components: {UIButton, UserInRecommended},
+  data() {
+    return {
+      API,
+      users: []
+    }
+  },
+  async created() {
+    this.users = this.$store.getters["users.store/sortedUsers"] ? this.$store.getters["users.store/sortedUsers"] : (await API.user.getAll()).data
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+@import "assets/local-styles/recommended-people-styles";
 </style>
