c91a05d224483efb82de5e76734c212e1438e068
nurseit <ns.ab06041999@gmail.com>
2020-09-23 12:08:30 +0600

    search album

--- front/pages/albums/index.vue
+++ front/pages/albums/index.vue
@@ -4,13 +4,17 @@
       <p class="ui-subtitle">Альбомы</p>
     </div>
     <div class="albums-page__filter">
-      <div class="mr-10"><input v-model="searchStr" @keyup="searchAlbum" placeholder="Введите для поиска" type="text"
-                                class="ui-input"></div>
-      <div class="filter-icon mr-10 cursor-pointer"><i class="material-icons md-36">sort_by_alpha</i></div>
-      <div class="filter-icon cursor-pointer"><i class="material-icons md-36">sort</i></div>
+      <div class="mr-10">
+        <input v-model="searchStr"
+               placeholder="Введите для поиска"
+               type="text"
+               class="ui-input">
+      </div>
+      <div @click="sortByAlph" class="filter-icon mr-10 cursor-pointer"><i class="material-icons md-36">sort_by_alpha</i></div>
+      <div @click="softById" class="filter-icon cursor-pointer"><i class="material-icons md-36">sort</i></div>
     </div>
     <div class="albums-page__list">
-      <albums :albums="albums"/>
+      <albums :albums="filteredAlbums"/>
     </div>
   </div>
 </template>
@@ -25,22 +29,35 @@ export default {
   data() {
     return {
       albums: [],
+      filteredAlbums: [],
       searchStr: ""
     }
   },
   watch: {
     async "searchStr" () {
+      this.searchAlbum();
       if (!this.searchStr.length) {
-        this.albums = (await API.albums.getAll()).data
+        this.filteredAlbums = (await API.albums.getAll()).data
       }
     }
   },
   async created() {
     this.albums = (await API.albums.getAll()).data
+    this.filteredAlbums = this.albums;
   },
   methods: {
     searchAlbum() {
-      this.albums = this.albums.find(el => el.title.toLowerCase().includes(this.searchStr.toLowerCase()));
+      this.filteredAlbums = this.albums.filter(el => {
+        if (el.title.includes(this.searchStr)) {
+          return el;
+        }
+      });
+    },
+    sortByAlph() {
+
+    },
+    softById() {
+
     }
   },
 }
